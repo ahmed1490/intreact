3397aaae2189500fb0b8c06f96856c5c6c28f69c
Andrea Moretti <andrea.moretti@zalando.de>
2016-03-30 12:51:55 +0200

    fix ref when element changes
    when element get unmounted or the ref changes the ref callback will
    return null, to handle that case we need to use the original reference
    when we get null (an original reference is always available as it is
    generated on the first rendering)

--- src/intreact.jsx
+++ src/intreact.jsx
@@ -38,7 +38,7 @@ export default class Intreact extends Component {
 
         if (hammerIsNeeded) {
             if (!canUseDOM) return;
-            this.hammer = new Hammer.Manager(this.refs.element, {
+            this.hammer = new Hammer.Manager(this._element, {
                 recognizers: this.getNeededRecognizers()
             });
             this.configureHammer();
@@ -97,7 +97,7 @@ export default class Intreact extends Component {
     }
 
     handleTapoutside(e) {
-        if (isContainedBy(e.target, this.refs.element)) return;
+        if (isContainedBy(e.target, this._element)) return;
         this.props.onTapoutside();
     }
 
@@ -134,7 +134,7 @@ export default class Intreact extends Component {
         }
 
         return (
-            <div {...childProps} ref="element">
+            <div {...childProps} ref={el => this._element = el ? el : this._element}>
                 {children}
             </div>
         );
